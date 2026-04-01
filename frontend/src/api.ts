86dbfbf7919a831a59142f5d92e52175b0b782cb
Levihenny <levi.henstrom@gmail.com>
2026-03-31 22:39:22 -0600

    fixes2

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,2 +1,3 @@
-export const API_BASE_URL =
-  'https://bookstore-henstrom-backend-d3hacbewh7b7hwak.centralus-01.azurewebsites.net';
+export const API_BASE_URL = import.meta.env.DEV
+  ? 'http://localhost:5043'
+  : 'https://bookstore-henstrom-backend-d3hacbewh7b7hwak.centralus-01.azurewebsites.net';
